5b78658d2c24aab2ea95afce228e66af8b4d477a
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-04-22 22:38:50 +0200

    terre02.js

---
+++ terre01.js
@@ -0,0 +1,3 @@
+const file = "terre01.js"
+const file2 = "terre02.js"
+console.log(file + "\n" + file2)
---
+++ terre02.js
@@ -0,0 +1,8 @@
+var txt = ""; {
+        txt += "J'apprend \n",
+        txt += "le \n"
+        txt += "javascript \n"
+        txt += "!"
+    }
+
+console.log(txt);
