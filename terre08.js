1c1cd4581f235f0fe02dd7ba5162eba06dccaa3e
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-04-23 23:22:55 +0200

    terre08.js

---
+++ terre08.js
@@ -0,0 +1,11 @@
+var readline = require("readline-sync");
+
+var nbr = readline.questionInt("Saisir un nombre ? ");
+var puissance = readline.questionInt("Saisir une puissance ? ");
+
+if(puissance < 0){
+var puissance2 = readline.questionInt("Saisir une puissance positive ");
+    console.log(nbr + " à la puissance " + puissance2 + " = " + Math.pow(nbr, puissance2)); 
+} else {
+    console.log(nbr + " à la puissance " + puissance + " = " + Math.pow(nbr, puissance)); 
+}
