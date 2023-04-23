631621f02caca8eeec9f45c4b71dba6755d6a849
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-04-23 23:58:47 +0200

    terre08.js

--- terre08.js
+++ terre08.js
@@ -4,8 +4,11 @@ var nbr = readline.questionInt("Saisir un nombre ? ");
 var puissance = readline.questionInt("Saisir une puissance ? ");
 
 if(puissance < 0){
-var puissance2 = readline.questionInt("Saisir une puissance positive ");
+    var puissance2 = readline.questionInt("Saisir une puissance positive ");
     console.log(nbr + " à la puissance " + puissance2 + " = " + Math.pow(nbr, puissance2)); 
+} else if (isNaN(nbr, puissance)){
+    let puissance2 = readline.questionInt("Saisir un nombre ? ");
+    console.log(nbr + " à la puissance " + puissance2 + " = " + Math.pow(nbr, puissance2));
 } else {
-    console.log(nbr + " à la puissance " + puissance + " = " + Math.pow(nbr, puissance)); 
+    console.log(nbr + " à la puissance " + puissance + " = " + Math.pow(nbr, puissance));
 }
