3e1cb94083cd9a13173d3de5316640ca9737de23
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-04-29 18:35:42 +0200

    terre10.js

---
+++ terre10.js
@@ -0,0 +1,16 @@
+var readline = require("readline-sync");
+
+var nbr = readline.question("Saisir un nombre premier ? ");
+
+function isPrime(nbr) {
+
+    if (nbr<=1) return false; 
+    for (var i = 2; i <= nbr-1; i++)
+        if (nbr% i == 0) return false;
+        console.log("Oui, " + nbr + " est un nombre premier . " + isPrime(nbr));
+            
+    return ;
+    console.log("Non, " + nbr + " n'est pas un nombre premier " + isPrime(nbr));
+} ;
+
+    
